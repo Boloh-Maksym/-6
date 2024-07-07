799e65c86a6061230c680e710c36fc07f4257d9f
Boloh-Maksym <bolohmaksym2005@gmail.com>
2024-07-07 13:58:36 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x1, y1, x2, y2;
+
+    printf("¬вед≥ть координати початку та к≥нц€ вектора (x1, y1, x2, y2): ");
+    scanf("%d %d %d %d", &x1, &y1, &x2, &y2);
+
+    double length = sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
+
+    printf("ƒовжина вектора: %.6f\n", length);
+
+    return 0;
+}
